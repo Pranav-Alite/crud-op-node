35d2710c8e73248bd4e507f2843f7a7dd7cb6004
pranav-mistry <pranav.alite@gmail.com>
2024-08-16 13:16:11 +0530

    made changes

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ connectDB
 
 const app = express()
 const PORT = process.env.PORT || 5000
-
-app.use(cors())
+// 
+app.use(cors()) 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
